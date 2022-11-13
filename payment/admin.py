b1fb4b3b3568fd31b4e1b342925df341f2f0d324
= <mmonir.m.29@gmail.com>
2022-11-13 13:04:01 +0600

    :sparkles: New admin site for Transection Model.

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,9 @@
+
 from django.contrib import admin
+from .models import Transection
 
 # Register your models here.
+
+@admin.register(Transection)
+class TransectionAdmin(admin.ModelAdmin):
+    list_display = ['tran_id', 'store_id', 'signature_key']
